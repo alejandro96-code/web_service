dbfe4e5664d79e13d03364738e48dae987061821
alejanr2 <alejanr2@student.42urduliz.com>
2025-11-07 15:00:38 +0100

    mejor comentario en el main

--- main.cpp
+++ main.cpp
@@ -2,26 +2,28 @@
 #include "parseoConf.hpp"
 #include "server.hpp"
 
+/*
+    Verificar que se proporcione el archivo de configuración
+    Leer configuración del archivo proporcionado
+    Verificar que se haya cargado la configuración correctamente
+    Mostrar configuración parseada
+    Crear e iniciar el servidor con la nueva configuración
+*/
 int main(int argc, char *argv[]) {
-    // Verificar que se proporcione el archivo de configuración
+    
     if (argc != 2) {
         std::cerr << "Uso: " << argv[0] << " [archivo de configuración]" << std::endl;
         return 1;
     }
     
-    // Leer configuración del archivo proporcionado
     std::vector<ServerConfig> servers = leerConfig(argv[1]);
-    
-    // Verificar que se haya cargado la configuración correctamente
     if (servers.empty()) {
         return 1;
     }
     
-    // Mostrar configuración parseada
     std::cout << "Configuración cargada:" << std::endl;
     std::cout << "  Número de servidores: " << servers.size() << std::endl << std::endl;
     
-    // Crear e iniciar el servidor con la nueva configuración
     Server servidor(servers[0]);
     servidor.iniciar();
     servidor.ejecutar();
