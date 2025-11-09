da69ec612c35d583b49d117aedf24062eb60601f
alejanr2 <alejanr2@student.42urduliz.com>
2025-11-09 18:14:39 +0100

    explicacion de los estados de codigo

--- includes/HttpStatus.hpp
+++ includes/HttpStatus.hpp
@@ -4,33 +4,38 @@
 #include <string>
 #include <map>
 
-class HttpStatus {
-public:
-    // Códigos de estado HTTP comunes
-    static const int OK = 200;
-    static const int CREATED = 201;
-    static const int NO_CONTENT = 204;
-    
-    static const int MOVED_PERMANENTLY = 301;
-    static const int FOUND = 302;
-    static const int NOT_MODIFIED = 304;
-    
-    static const int BAD_REQUEST = 400;
-    static const int FORBIDDEN = 403;
-    static const int NOT_FOUND = 404;
-    static const int METHOD_NOT_ALLOWED = 405;
-    static const int PAYLOAD_TOO_LARGE = 413;
-    
-    static const int INTERNAL_SERVER_ERROR = 500;
-    static const int NOT_IMPLEMENTED = 501;
-    static const int HTTP_VERSION_NOT_SUPPORTED = 505;
-    
-    // Obtener mensaje de estado para un código
-    static std::string getMessage(int code);
-    
-private:
-    static std::map<int, std::string> initMessages();
-    static std::map<int, std::string> _messages;
+/*
+    Codigos de estado:
+    Son codigo de 3 digitos que indican el resultado de una solicitud al servidor
+    Los codigo 20X indican que la solicitud a sido correcta
+        200 solicitud exitosa
+        201 solicitud bien creada
+    Los codigos 400 indican un error del cliente
+        400 solicitud mal creada
+        403 solicitud hecha sin tener permisos
+        404 archivo, ruta... no encontrada
+        405 solicitud de un metodo no permitido
+    Los codigos 500 indican error en el servidor
+        500 error interno (la solicitud esta bien pero algo falla para realizarla)
+
+    Tambien existen los codigo 30x aunque aqui no los usamos (codigos de redireccion)
+        que indican que el recurso requiere una accion adicional o se ha movido
+*/
+
+class HttpStatus
+{
+    public:
+        static const int OK = 200;
+        static const int CREATED = 201;
+        static const int BAD_REQUEST = 400;
+        static const int FORBIDDEN = 403;
+        static const int NOT_FOUND = 404;
+        static const int METHOD_NOT_ALLOWED = 405;
+        static const int INTERNAL_SERVER_ERROR = 500;
+        static std::string getMessage(int code);
+    private:
+        static std::map<int, std::string> initMessages();
+        static std::map<int, std::string> _messages;
 };
 
 #endif
