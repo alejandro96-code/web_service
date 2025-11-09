5278139f8febe7a2dcdc8a57dcf121f618e01f21
alejanr2 <alejanr2@student.42urduliz.com>
2025-11-09 18:48:24 +0100

    informacion en el cpp

--- includes/HttpStatus.hpp
+++ includes/HttpStatus.hpp
@@ -4,24 +4,6 @@
 #include <string>
 #include <map>
 
-/*
-    Codigos de estado:
-    Son codigo de 3 digitos que indican el resultado de una solicitud al servidor
-    Los codigo 20X indican que la solicitud a sido correcta
-        200 solicitud exitosa
-        201 solicitud bien creada
-    Los codigos 400 indican un error del cliente
-        400 solicitud mal creada
-        403 solicitud hecha sin tener permisos
-        404 archivo, ruta... no encontrada
-        405 solicitud de un metodo no permitido
-    Los codigos 500 indican error en el servidor
-        500 error interno (la solicitud esta bien pero algo falla para realizarla)
-
-    Tambien existen los codigo 30x aunque aqui no los usamos (codigos de redireccion)
-        que indican que el recurso requiere una accion adicional o se ha movido
-*/
-
 class HttpStatus
 {
     public:
